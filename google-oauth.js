e6b079f1f000061fcbb00f70af242d32d2f0e339
AditiSujlegaonkar1697 <aditisujlegaonkar@gmail.com>
2023-11-01 22:24:05 +0530

    google oauth backend url update

--- google-oauth.js
+++ google-oauth.js
@@ -10,7 +10,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
-        "https://time-trace-backend.onrender.com/auth/google/callback",
+        "https://time-trace-backend3.onrender.com/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       // let email = profile._json.email
